8115cf2415b3914508905b78983ffb8877bce280
DESKTOP-PLP1QUM\Administrator <1852511683@qq.com>
2020-07-21 10:21:48 +0800

    2020年7月21

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,51 @@
+<!--  -->
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item"
+         v-for="(item,index) in titles"
+         :key="index"
+         :class="{active:index===currentIndex}"
+         @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    };
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index;
+    }
+  }
+};
+</script>
+<style scoped>
+.tab-control {
+  display: flex;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  font-size: 15px;
+}
+.tab-control-item {
+  flex: 1;
+}
+.tab-control .active span {
+  color: #fb7299;
+  border-bottom: 3px solid #fb7299;
+  padding: 5px;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -5,22 +5,141 @@
       <div slot="center">购物街</div>
     </NavBar>
     <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <Feature />
+    <TabControl class="tab-control"
+                :titles="['流行','新款','精选']" />
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+
 import HomeSwiper from "./chilsComps/HomeSwiper";
-import { getHomeMultidata } from "../../network/home.js";
+import RecommendView from "./chilsComps/HomeRecommendView";
+import Feature from "./chilsComps/FeatureView";
+
+import { getHomeMultidata, getHomeGoods } from "../../network/home.js";
 export default {
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView,
+    Feature,
+    TabControl
   },
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        news: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      }
     };
   },
   created() {
@@ -29,12 +148,27 @@ export default {
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
     });
+    getHomeGoods("pop", 1).then(res => {
+      console.log(res);
+    });
   }
 };
 </script>
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 100%;
   background-color: #fb7299;
   color: #fff;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
